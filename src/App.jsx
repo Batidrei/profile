263bfe815e044385a74bf1922f96d74c6e870655
AlexAndrei04 <bastidaflores@gmail.com>
2021-01-24 21:21:55 -0600

    Add basename

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './scss/main.scss'
-import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Home } from './Components/Pages/Home'
 import { Knowdledge } from './Components/Pages/Knowdledge'
 import TimelinePage from './Components/Pages/TimelinePage'
 import { Container } from 'react-bootstrap'
 
 const App = () => (
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <Container className='back--in-down'>
       <Switch>
         <Route path='/' exact component={Home} />
