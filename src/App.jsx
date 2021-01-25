b7740a1e8ee2f60def4a33e53db4b0863192f6d8
AlexAndrei04 <bastidaflores@gmail.com>
2021-01-24 21:47:24 -0600

    Delete extra routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import './scss/main.scss'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Home } from './Components/Pages/Home'
-import { Knowdledge } from './Components/Pages/Knowdledge'
-import TimelinePage from './Components/Pages/TimelinePage'
 import { Container } from 'react-bootstrap'
 
 const App = () => (
@@ -11,8 +9,6 @@ const App = () => (
     <Container className='back--in-down'>
       <Switch>
         <Route path='/' exact component={Home} />
-        <Route path='/knowdledge' exact component={Knowdledge} />
-        <Route path='/timeline' exact component={TimelinePage} />
       </Switch>
     </Container>
   </Router>
