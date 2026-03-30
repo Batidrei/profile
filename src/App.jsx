3798aedf61f0f70ec5bb03aae888aea846c8ea66
Alex Andrei <bastidaflores@gmail.com>
2026-03-30 15:46:16 -0600

    Improve Data Analytics to check the path

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
 // src/App.jsx
-import React from 'react';
+import React, { useEffect } from 'react';
 import './scss/main.scss';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga4';
 
 import { Home } from './components/pages/home';
 import { Portfolio } from './components/pages/portfolio';
 import { Navigation } from './components/organims/navigation';
-// Asegúrate de exportarlo en PascalCase desde el archivo
-// export const Error404 = () => ( ... )
 import { Error404 } from './components/pages/error404';
+
+const TRACKING_ID = "G-011EDV0ZP1";
+ReactGA.initialize(TRACKING_ID);
+
+// Componente para rastrear cambios de ruta
+const PageTracker = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const pagePath = location.pathname + location.hash;
+
+    ReactGA.send({
+      hitType: "pageview",
+      page: pagePath,
+      title: document.title
+    });
+  }, [location]);
+
+  return null;
+};
 
 const App = () => (
   <Router>
+    <PageTracker />
     <div className="back--in-down">
       <Navigation />
       <Routes>
